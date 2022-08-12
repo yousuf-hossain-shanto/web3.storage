e1a59cce48f1bd475c4d74d0f1981315e226fe2b
Yousuf Hossain <yousuf.hossain.shanto@gmail.com>
2022-08-12 23:51:09 +0600

    added idb store from browser

--- packages/client/src/platform.web.js
+++ packages/client/src/platform.web.js
@@ -1,9 +1,8 @@
-// TODO: Use indexedDb
-import { MemoryBlockStore } from 'ipfs-car/blockstore/memory'
+import { IdbBlockStore } from 'ipfs-car/blockstore/idb'
 
 export const fetch = globalThis.fetch
 export const Request = globalThis.Request
 export const Response = globalThis.Response
 export const Blob = globalThis.Blob
 export const File = globalThis.File
-export const Blockstore = MemoryBlockStore
+export const Blockstore = IdbBlockStore
